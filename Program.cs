7f1a723b7314e56f664d5658b9fc0e4f08ade394
Darwin Rodríguez <darwinrv94@gmail.com>
2022-06-14 19:49:03 -0400

    Remove ignored files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyecto;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(options => options.UseInMemoryDatabase("TareasDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", ([FromServices] TareasContext dbContext) => {
+  dbContext.Database.EnsureCreated();
+
+  return Results.Ok("Base de datos en memoria " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
